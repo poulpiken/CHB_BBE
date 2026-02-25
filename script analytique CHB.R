d9961653c552e8bf1cf5f344d26d8fec99e37ba2
Pauline Liliane Thérèse Thibodot <Pauline.Thibodot@unige.ch>
2026-02-25 13:50:26 +0100

    aller on envoie la sauce

---
+++ script analytique CHB.R
@@ -0,0 +1,372 @@
+#----- Script analytique -----
+# Ce script identifie les trois groupes d'intérêt BB, contrôle positif et contrôle négatif et les compare
+
+#----- Chargement des packages nécessaires -----
+
+library(broom)
+library(dplyr)
+library(forcats)
+library(forestplot)
+library(ggalluvial)
+library(ggplot2)
+library(ggpubr)
+library(gtsummary)
+library(gt)
+library(haven)
+library(lubridate)
+library(paletteer)
+library(purrr)
+library(readxl)
+library(splines)
+library(stringr)
+library(survival)
+library(survminer)
+library(tibble)
+library(tidyr)
+
+#----- Création des 3 groupes d'intérêt -----
+
+# Groupe BB contient les very old donor ayant donné aux very young recipient et young recipient
+BB_group <- data_CHB_BBE %>%
+  filter((don_age >= 70 & rec_age_at_tx < 20) | (don_age >= 70 & rec_age_at_tx >= 20 & rec_age_at_tx < 40))
+BB_group1 <- data_CHB_BBE %>%
+  filter(don_age >= 70 & rec_age_at_tx < 20)
+BB_group2 <- data_CHB_BBE %>%
+  filter(don_age >= 70 & rec_age_at_tx >= 20 & rec_age_at_tx < 40)
+
+# Groupe contrôle positif contient les very young donor ayant donné aux very young recipient et young recipient
+pos_ctrl_group <- data_CHB_BBE %>%
+  filter((don_age < 20 & rec_age_at_tx < 20) | (don_age < 20 & rec_age_at_tx >= 20 & rec_age_at_tx < 40))
+pos_ctrl_group1 <- data_CHB_BBE %>%
+  filter(don_age < 20 & rec_age_at_tx < 20)
+pos_ctrl_group2 <- data_CHB_BBE %>%
+  filter(don_age < 20 & rec_age_at_tx >= 20 & rec_age_at_tx < 40)
+
+# Groupe contrôle négatif contient les very old donor ayant donné aux very old recipient et old recipient
+neg_ctrl_group <- data_CHB_BBE %>%
+  filter((don_age >= 70 & rec_age_at_tx >= 70) | (don_age >= 70 & rec_age_at_tx >= 60 & rec_age_at_tx < 70))
+neg_ctrl_group1 <- data_CHB_BBE %>%
+  filter(don_age >= 70 & rec_age_at_tx >= 70)
+neg_ctrl_group2 <- data_CHB_BBE %>%
+  filter(don_age >= 70 & rec_age_at_tx >= 60 & rec_age_at_tx < 70)
+
+#----- Table 1 de nos groupes d'intérêt -----
+#Faire un tableau descriptif des variables rec_age_at_tx, don_age, rec_gender, don_gender, don_cod, rec_hgt_cm, 
+#rec_wgt_kg, don_hgt_cm, don_wgt_kg
+#sur l'effectif total et les trois groupes d'intérêt BB group, Negative control et Positive control avec une
+#comparaison entre les 3 groupes et une pvalue sur la colonnes la plus à droite
+
+table1 <- bind_rows(
+  BB_group       %>% mutate(Group = "BB group"),
+  pos_ctrl_group %>% mutate(Group = "Positive control"),
+  neg_ctrl_group %>% mutate(Group = "Negative control"),
+  data_CHB_BBE %>% mutate(Group = "All patients")
+) %>%
+  mutate(
+    Group = factor(
+      Group,
+      levels = c(
+        "BB group",
+        "Positive control",
+        "Negative control",
+        "All patients"
+      )
+    )
+  ) %>%
+  select(
+    Group,
+    rec_age_at_tx, don_age, rec_gender, don_gender, don_cod, rec_hgt_cm, rec_wgt_kg, don_hgt_cm, don_wgt_kg
+  ) %>%
+  tbl_summary(
+    by = Group,
+    missing = "no",
+    statistic = list(
+      all_continuous() ~ "{median}",
+      all_categorical() ~ "{n} ({p}%)"
+    ),
+    digits = all_continuous() ~ 0,
+    label = list(
+      rec_age_at_tx ~ "Recipient age at transplant",
+      rec_gender ~ "Recipient gender",
+      rec_hgt_cm ~ "Recipient height in cm",
+      rec_wgt_kg ~ "Recipient weight in kg",
+      don_age ~ "Donor age",
+      don_gender ~ "Donor gender",
+      don_hgt_cm ~ "Donor height in cm",
+      don_wgt_kg ~ "Donor weight in kg",
+      don_cod ~ "Cause of donor death"
+    )
+  ) %>%
+  add_p() %>%
+  modify_header(label = "**Variable**") %>%
+  modify_spanning_header(
+    all_stat_cols() ~ "**Group**"
+  )
+# Affichage du tableau
+table1_gt <- as_gt(table1)
+print(table1_gt)
+
+#----- Comparaison des groupes d'intérêt -----
+comparison_data <- bind_rows(
+  BB_group1       %>% mutate(Group = "Very old donor -> Very young recipient"),
+  BB_group2       %>% mutate(Group = "Very old donor -> Young recipient"),
+  pos_ctrl_group1 %>% mutate(Group = "Very young donor -> Very young recipient"),
+  pos_ctrl_group2 %>% mutate(Group = "Very young donor -> Young recipient"),
+  neg_ctrl_group1 %>% mutate(Group = "Very old donor -> Very old recipient"),
+  neg_ctrl_group2 %>% mutate(Group = "Very old donor -> Old recipient")
+)
+
+# Calcul médiane et quartiles des durées graft_surv_years pour les 3 groupes
+summary_df <- comparison_data %>%
+  group_by(Group) %>%
+  summarise(
+    median = median(graft_surv_years, na.rm = TRUE),
+    q1     = quantile(graft_surv_years, 0.25, na.rm = TRUE),
+    q3     = quantile(graft_surv_years, 0.75, na.rm = TRUE),
+    .groups = "drop"
+  )
+summary_df$Group <- factor(
+  summary_df$Group,
+  levels = c("Very young donor -> Young recipient", "Very young donor -> Very young recipient", "Very old donor -> Old recipient", "Very old donor -> Very old recipient", "Very old donor -> Young recipient", "Very old donor -> Very young recipient")
+)
+
+# Forest plot
+ggplot(summary_df, aes(x = median, y = Group, color = Group)) +
+  geom_point(size = 3) +
+  geom_errorbar(
+    aes(xmin = q1, xmax = q3),
+    width = 0.2
+  ) +
+  scale_color_manual(
+    values = c(
+      "Very old donor -> Very young recipient" = "#2e15d1",
+      "Very old donor -> Young recipient" = "#2e15d1",
+      "Very young donor -> Very young recipient" = "#15D12E",
+      "Very young donor -> Young recipient" = "#15D12E",
+      "Very old donor -> Very old recipient" = "#d12e15",
+      "Very old donor -> Old recipient" = "#d12e15"
+    ),
+    breaks = c(
+      "Very old donor -> Very young recipient",
+      "Very young donor -> Very young recipient",
+      "Very old donor -> Very old recipient"
+    ),
+    labels = c(
+      "BB group",
+      "Positive control",
+      "Negative control"
+    )
+  ) +
+  labs(
+    x = "Graft survival (years)",
+    y = "",
+    title = "Graft survival - Median + IQR",
+    color = ""
+  ) +
+  theme_minimal()
+
+# Sauvegarde du graphique
+ggsave("graft_survival_forest_plot.png", width = 8, height = 6)
+
+
+#----- Etude survie patients selon les 3 groupes -----
+#créer un nouveau dataframe avec les groupes Bb group, pos et neg control
+comparison_data <- bind_rows(
+  BB_group       %>% mutate(Group = "BB group"),
+  pos_ctrl_group %>% mutate(Group = "Positive control"),
+  neg_ctrl_group %>% mutate(Group = "Negative control")
+)
+comparison_data <- comparison_data %>%
+  mutate(
+    Group = factor(
+      Group,
+      levels = c(
+        "BB group",
+        "Positive control",
+        "Negative control"
+      )
+    )
+  )
+
+# Kaplan Meier plot sur graft_surv_years
+km_fit_graft <- survfit(Surv(graft_surv_years, graft_event) ~ Group, data = comparison_data)
+km_plot_graft <- ggsurvplot(
+  km_fit_graft,
+  data = comparison_data,
+  surv.median.line = "hv",
+  risk.table = TRUE,
+  pval = TRUE,
+  conf.int = TRUE,
+  xlab = "Time in years",
+  ylab = "Graft survival probability",
+  title = "Kaplan-Meier Graft Survival Curve by Group",
+  legend.title = "Group",
+  legend.labs = c(
+    "BB group",
+    "Positive control",
+    "Negative control"
+  ),
+  palette = c(
+    "#2e15d1",
+    "#15D12E",
+    "#d12e15"
+  )
+)
+# Affichage du graphique
+print(km_plot_graft)
+# Sauvegarde du graphique
+ggsave("km_graft_survival.png", plot = km_plot_graft$plot, width = 8, height = 6)
+# Sauvegarde du tableau des risques
+ggsave("km_graft_survival_risk_table.png", plot = km_plot_graft$table, width = 8, height = 4)
+
+#----- Survie greffon BB vs negative control -----
+#Courbe de Kaplan Meier comparant uniquement le groupe BB et le groupe contrôle négatif
+bb_neg_ctrl_data_graft <- bind_rows(
+  BB_group       %>% mutate(Group = "BB group"),
+  neg_ctrl_group %>% mutate(Group = "Negative control")
+)
+bb_neg_ctrl_data_graft <- bb_neg_ctrl_data_graft %>%
+  mutate(
+    Group = factor(
+      Group,
+      levels = c(
+        "BB group",
+        "Negative control"
+      )
+    )
+  )
+#Kaplan Meier plot sur graft_surv_years
+km_fit_bb_neg_graft <- survfit(Surv(graft_surv_years, graft event) ~ Group, data = bb_neg_ctrl_data_graft)
+km_plot_bb_neg_graft <- ggsurvplot(
+  km_fit_bb_neg_graft,
+  data = bb_neg_ctrl_data_graft,
+  surv.median.line = "hv",
+  risk.table = TRUE,
+  pval = TRUE,
+  conf.int = TRUE,
+  xlab = "Time in years",
+  ylab = "Graft survival probability",
+  title = "Kaplan-Meier Graft Survival Curve: BB group vs Negative control",
+  legend.title = "Group",
+  legend.labs = c(
+    "BB group",
+    "Negative control"
+  ),
+  palette = c(
+    "#2e15d1",
+    "#d12e15"
+  )
+)
+# Affichage du graphique
+print(km_plot_bb_neg_graft)
+# Sauvegarde du graphique
+ggsave("km_graft_survival_plot_bb_vs_neg_control.png", plot = km_plot_bb_neg_graft$plot, width = 8, height = 6)
+# Sauvegarde du tableau des risques
+ggsave("km_graft_survival_risk_table_bb_vs_neg_control.png", plot = km_plot_bb_neg_graft$table, width = 8, height = 4)
+
+#Comparaison BB group vs negative control à partir de 5 ans
+bb_neg_ctrl_data_graft_5yrs <- bb_neg_ctrl_data_graft %>%
+  filter(graft_surv_years >= 5) %>%
+  mutate(time_since_5 = graft_surv_years - 5)
+
+#Kaplan Meier plot sur graft_surv_years à partir de 5 ans
+km_fit_bb_neg_graft_5yrs <- survfit(
+  Surv(time_since_5, graft_event) ~ Group,
+  data = bb_neg_ctrl_data_graft_5yrs
+)
+km_plot_bb_neg_graft_5yrs <- ggsurvplot(
+  km_fit_bb_neg_graft_5yrs,
+  data = bb_neg_ctrl_data_graft_5yrs,
+  surv.median.line = "hv",
+  risk.table = TRUE,
+  pval = TRUE,
+  conf.int = TRUE,
+  xlab = "Time since 5 years (years)",
+  ylab = "Graft survival probability",
+  title = "Kaplan–Meier Graft survival from 5 years: BB group vs Negative control",
+  legend.title = "Group",
+  legend.labs = c("BB group", "Negative control"),
+  palette = c("#2e15d1", "#d12e15")
+)
+# Affichage du graphique
+print(km_plot_bb_neg_graft_5yrs)
+# Sauvegarde du graphique
+ggsave("km_graft_survival_plot_bb_vs_neg_control_5yrs.png", plot = km_plot_bb_neg_graft_5yrs$plot, width = 8, height = 6)
+# Sauvegarde du tableau des risques
+ggsave("km_graft_survival_risk_table_bb_vs_neg_control_10yrs.png", plot = km_plot_bb_neg_graft_5yrs$table, width = 8, height = 4)
+
+#----- Survie greffon BB group vs positive control -----
+#Courbe de Kaplan Meier comparant uniquement le groupe BB et le groupe contrôle positif
+bb_pos_ctrl_data_graft <- bind_rows(
+  BB_group       %>% mutate(Group = "BB group"),
+  pos_ctrl_group %>% mutate(Group = "Positive control")
+)
+bb_pos_ctrl_data_graft <- bb_pos_ctrl_data_graft %>%
+  mutate(
+    Group = factor(
+      Group,
+      levels = c(
+        "BB group",
+        "Positive control"
+      )
+    )
+  )
+#Kaplan Meier plot sur graft_surv_years
+km_fit_bb_pos_graft <- survfit(Surv(graft_surv_years, graft_event) ~ Group, data = bb_pos_ctrl_data_graft)
+km_plot_bb_pos_graft <- ggsurvplot(
+  km_fit_bb_pos_graft,
+  data = bb_pos_ctrl_data_graft,
+  surv.median.line = "hv",
+  risk.table = TRUE,
+  pval = TRUE,
+  conf.int = TRUE,
+  xlab = "Time in years",
+  ylab = "Graft survival probability",
+  title = "Kaplan-Meier Graft Survival Curve: BB group vs Positive control",
+  legend.title = "Group",
+  legend.labs = c(
+    "BB group",
+    "Positive control"
+  ),
+  palette = c(
+    "#2e15d1",
+    "#15D12E"
+  )
+)
+# Affichage du graphique
+print(km_plot_bb_pos_graft)
+# Sauvegarde du graphique
+ggsave("km_graft_survival_plot_bb_vs_pos_control.png", plot = km_plot_bb_pos_graft$plot, width = 8, height = 6)
+# Sauvegarde du tableau des risques
+ggsave("km_graft_survival_risk_table_bb_vs_pos_control.png", plot = km_plot_bb_pos_graft$table, width = 8, height = 4)
+
+#Comparaison BB group vs positive control à partir de 5 ans
+bb_pos_ctrl_data_graft_5yrs <- bb_pos_ctrl_data_graft %>%
+  filter(graft_surv_years >= 5) %>%
+  mutate(time_since_5 = graft_surv_years - 5)
+
+#Kaplan Meier plot sur graft_surv_years à partir de 5 ans
+km_fit_bb_pos_graft_5yrs <- survfit(
+  Surv(time_since_5, graft_event) ~ Group,
+  data = bb_pos_ctrl_data_graft_5yrs
+)
+km_plot_bb_pos_graft_5yrs <- ggsurvplot(
+  km_fit_bb_pos_graft_5yrs,
+  data = bb_pos_ctrl_data_graft_5yrs,
+  surv.median.line = "hv",
+  risk.table = TRUE,
+  pval = TRUE,
+  conf.int = TRUE,
+  xlab = "Time since 5 years (years)",
+  ylab = "Graft survival probability",
+  title = "Kaplan–Meier Graft survival from 5 years: BB group vs Positive control",
+  legend.title = "Group",
+  legend.labs = c("BB group", "Positive control"),
+  palette = c("#2e15d1", "#15D12E")
+)
+# Affichage du graphique
+print(km_plot_bb_pos_graft_5yrs)
+# Sauvegarde du graphique
+ggsave("km_graft_survival_plot_bb_vs_pos_control_5yrs.png", plot = km_plot_bb_pos_graft_5yrs$plot, width = 8, height = 6)
+# Sauvegarde du tableau des risques
+ggsave("km_graft_survival_risk_table_bb_vs_pos_control_5yrs.png", plot = km_plot_bb_pos_graft_5yrs$table, width = 8, height = 4)
--- script descriptif CHB.R
+++ script descriptif CHB.R
@@ -62,7 +62,210 @@ data_CHB_BBE <- data_CHB_BBE %>%
          graft_failure, rec_gender, rec_birth_dt, mult_tx, rec_hgt_cm, rec_wgt_kg, bmi, graft_cumulated_age,
     don_gender, don_hgt_cm, don_wgt_kg, don_cod)
 
-#exclure les patients avec mult_tx = O
+#exclure les patients multitransplantés mult_tx = O
 data_CHB_BBE <- data_CHB_BBE %>%
-  filter(mult_tx == "O")
+  filter(mult_tx == "N")
 
+#exclure les patients avec graft failure < 30 jours
+data_CHB_BBE <- data_CHB_BBE %>%
+  filter(graft_surv_days >= 30)
+
+#exclure les patients < 18 ans
+data_CHB_BBE <- data_CHB_BBE %>%
+  filter(rec_age_at_tx >= 18)
+
+#Division du dataset en période de 4 ans
+data_CHB_BBE <- data_CHB_BBE %>%
+  mutate(tx_period = year(rec_tx_dt))
+data_CHB_BBE <- data_CHB_BBE %>%
+  mutate(tx_period = case_when(
+    tx_period >= 1993 & tx_period <= 1997 ~ "1993-1997",
+    tx_period >= 1998 & tx_period <= 2002 ~ "1998-2002",
+    tx_period >= 2003 & tx_period <= 2007 ~ "2003-2007",
+    tx_period >= 2008 & tx_period <= 2012 ~ "2008-2012",
+    tx_period >= 2013 & tx_period <= 2017 ~ "2013-2017",
+    tx_period >= 2018 & tx_period <= 2024 ~ "2018-2024",
+    TRUE ~ NA_character_
+  ))
+
+#Ajout d'une colonne don_age_grp avec les 5 groupes différents d'âge de donneur : < 20 ans, 20-40 ans
+#40 - 60 ans, 60-70 ans et > 70 ans
+data_CHB_BBE <- data_CHB_BBE %>%
+  mutate(don_age_grp = case_when(
+    don_age < 20 ~ "Very young donor",
+    don_age >= 20 & don_age < 40 ~ "Young donor",
+    don_age >= 40 & don_age < 60 ~ "Middle-aged donor",
+    don_age >= 60 & don_age < 70 ~ "Old donor",
+    don_age >= 70 ~ "Very old donor",
+    TRUE ~ NA_character_
+  ))
+
+# créer un facteur pour don_age_grp avec les niveaux dans l'ordre souhaité
+data_CHB_BBE$don_age_grp <- factor(data_CHB_BBE$don_age_grp, levels = c("Very young donor", "Young donor", 
+                                                                        "Middle-aged donor", "Old donor", "Very old donor"))
+data_CHB_BBE$don_age_grp <- factor(
+  data_CHB_BBE$don_age_grp,
+  levels = rev(levels(data_CHB_BBE$don_age_grp))
+)    
+
+#Ajout d'une colonne rec_age_grp avec les 5 groupes différetnts d'âge de receveur : < 20 ans, 20-40 ans
+#40 - 60 ans, 60-70 ans et > 70 ans
+data_CHB_BBE <- data_CHB_BBE %>%
+  mutate(rec_age_grp = case_when(
+    rec_age_at_tx < 20 ~ "Very young recipient",
+    rec_age_at_tx >= 20 & rec_age_at_tx < 40 ~ "Young recipient",
+    rec_age_at_tx >= 40 & rec_age_at_tx < 60 ~ "Middle-aged recipient",
+    rec_age_at_tx >= 60 & rec_age_at_tx < 70 ~ "Old recipient",
+    rec_age_at_tx >= 70 ~ "Very old recipient",
+    TRUE ~ NA_character_
+  ))
+
+# créer un facteur pour rec_age_grp avec les niveaux dans l'ordre souhaité
+data_CHB_BBE$rec_age_grp <- factor(data_CHB_BBE$rec_age_grp, levels = c("Very young recipient", "Young recipient", 
+                                                                        "Middle-aged recipient", "Old recipient", "Very old recipient"))
+data_CHB_BBE$rec_age_grp <- factor(
+  data_CHB_BBE$rec_age_grp,
+  levels = rev(levels(data_CHB_BBE$rec_age_grp))
+)
+
+#Renommer les valeurs de CAN_GENDER et DON_GENDER dans l'affichage 
+#F = Women et M = Men
+data_CHB_BBE <- data_CHB_BBE |> 
+  mutate(rec_gender = case_when(
+    rec_gender == "F" ~ "Women",
+    rec_gender == "M" ~ "Men"
+  ))
+
+data_CHB_BBE <- data_CHB_BBE |> 
+  mutate(don_gender = case_when(
+    don_gender == "F" ~ "Women",
+    don_gender == "M" ~ "Men"
+  ))
+
+#Renommer les valeurs de don_cod
+#Anoxie = Anoxia, Traumatique A.V.P. et Traumatique Non A.V.P. = Head trauma, Vasculaire = Cerebrovascular accident or stroke,
+#tous les autres = Other
+data_CHB_BBE <- data_CHB_BBE |> 
+  mutate(don_cod = case_when(
+    don_cod == "Anoxie" ~ "Anoxia",
+    don_cod == "Traumatique A.V.P." ~ "Head trauma",
+    don_cod == "Traumatique Non A.V.P." ~ "Head trauma",
+    don_cod == "Vasculaire" ~ "Cerebrovascular accident or stroke",
+    TRUE ~ "Other"
+  ))
+
+#----- Squvegarde du dataset final ------
+save(data_CHB_BBE, file = "data_CHB_BBE.RData")
+
+#----- Données sur les groupes d'âge des donneurs et des receveurs -----
+
+#Tableau des groupes d'âge des donneurs et des receveurs en nombre et pourcentage
+table_age_groups <- data_CHB_BBE %>%
+  select(don_age_grp, rec_age_grp) %>%
+  tbl_summary(
+    by = NULL,
+    statistic = list(all_categorical() ~ "{n} ({p}%)"),
+    missing = "no",
+    label = list(don_age_grp ~ "Donor Age Group", rec_age_grp ~ "Recipient Age Group")
+  ) %>%
+  add_overall() %>%
+  modify_header(label ~ "**Age Groups**") %>%
+  bold_labels()
+#affichage et sauvegarde du tableau
+table_age_groups |> as_gt() |> gt::gtsave("table_age_groups.png")
+
+#tableau répartition des groupes de donneurs selon le groupe d'âge des receveurs
+table_donor_by_recipient_age <- data_CHB_BBE %>%
+  select(don_age_grp, rec_age_grp) %>%
+  tbl_summary(
+    by = rec_age_grp,
+    statistic = list(all_categorical() ~ "{n} ({p}%)"),
+    missing = "no",
+    label = list(don_age_grp ~ "Donor Age Group")
+  ) %>%
+  add_overall() %>%
+  modify_header(label ~ "**Donor Age Groups by Recipient Age Groups**") %>%
+  bold_labels()
+
+#affichage et sauvegarde du tableau
+table_donor_by_recipient_age |> as_gt() |> gt::gtsave("table_donor_by_recipient_age.png")
+
+#Histogramme pour visualiser les âges des donneurs selon les 5 groupes
+#data_CHB_BBE$don_age_grp <- fct_relevel(data_CHB_BBE$don_age_grp, "Very young donor", "Young donor", "Middle-aged donor", "Old donor", "Very old donor")
+
+ggplot(data_CHB_BBE, aes(x = don_age_grp))+
+  geom_bar(fill = "#246B45", color = "black")+
+  labs(title = "Distribution of Donor Ages by Age Group",
+       x = "Donor Age Group",
+       y = "Count of Donors")+
+  theme_minimal()
+ggsave("donor_age_distribution.png", width = 8, height = 6)
+
+#Histogramme pour visualiser les âges des receveurs selon les 5 groupes
+#data_CHB_BBE$rec_age_grp <- fct_relevel(data_CHB_BBE$rec_age_grp, "Very young recipient", "Young recipient", "Middle-aged recipient", "Old recipient", "Very old recipient")
+ggplot(data_CHB_BBE, aes(x = rec_age_grp))+
+  geom_bar(fill = "#ED912F", color = "black")+
+  labs(title = "Distribution of Recipient Ages by Age Group",
+       x = "Recipient Age Group",
+       y = "Count of Recipients")+
+  theme_minimal()
+ggsave("recipient_age_distribution.png", width = 8, height = 6)
+
+#histogramme pour voir la distribution des ages de donneur selon des periodes de 5 ans de 1993 à 2024
+
+# créer un facteur pour tx_period avec les niveaux dans l'ordre souhaité
+data_CHB_BBE$tx_period <- factor(data_CHB_BBE$tx_period, levels = c("1993-1997", "1998-2002", "2003-2007", "2008-2012", "2013-2017", "2018-2024"))
+
+# histogramme en nombre
+ggplot(data_CHB_BBE, aes(x = tx_period, fill = don_age_grp)) +
+  geom_bar(color = "black")+
+  scale_fill_manual(values = paletteer_d("NineteenEightyR::miami2", n = 5))+    #palette trouvée sur Color Palette Finder
+  labs(title = "Distribution of Donor Ages by Age Group and Transplant Period",
+       x = "Donor Age Group",
+       y = "Count of Donors",... = "Transplant Period")+
+  theme_minimal()
+
+ggsave("donor_age_by_period.png", width = 10, height = 6)
+
+# histogramme en pourcentages
+ggplot(data_CHB_BBE, aes(x = tx_period, fill = don_age_grp)) +
+  geom_bar(position = "fill", color = "black") +
+  scale_fill_manual(values = paletteer_d("NineteenEightyR::miami2", n = 5))+
+  scale_y_continuous(labels = scales::percent) +
+  labs(
+    title = "Distribution of donor age groups by transplant period",
+    x = "Transplant period",
+    y = "Proportion of donors",
+    fill = "Donor age group"
+  ) +
+  theme_minimal()
+ggsave("donor_age_by_period_percentage.png", width = 10, height = 6)
+
+#histogramme pour voir la distribution des ages de receveur selon des periodes de 5 ans de 1993 à 2024
+
+#Histogramme en nombre
+ggplot(data_CHB_BBE, aes(x = tx_period, fill = rec_age_grp)) +
+  geom_bar(color = "black")+
+  scale_fill_manual(values = paletteer_d("lisa::FernandoBotero", n = 5))+
+  labs(title = "Distribution of Recipient Ages by Age Group and Transplant Period",
+       x = "Transplant Period",
+       y = "Count of Recipients",... = "Transplant Period")+
+  theme_minimal()
+ggsave("recipient_age_by_period.png", width = 10, height = 6)
+
+#histogramme en pourcentage
+ggplot(data_CHB_BBE, aes(x = tx_period, fill = rec_age_grp)) +
+  geom_bar(position = "fill", color = "black") +
+  scale_fill_manual(values = paletteer_d("lisa::FernandoBotero", n = 5))+
+  scale_y_continuous(labels = scales::percent) +
+  labs(
+    title = "Distribution of recipient age groups by transplant period",
+    x = "Transplant period",
+    y = "Proportion of recipients",
+    fill = "Recipient age group"
+  ) +
+  theme_minimal()
+ggsave("recipient_age_by_period_percentage.png", width = 10, height = 6)
+
+
+# ----- Fin du script -----
